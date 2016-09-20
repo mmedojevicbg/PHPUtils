a4c8b7378a481739b1bf467d9b26c1b58f660f68
mmedojevicbg <mmedojevicbg@gmail.com>
2016-09-20 14:28:42 +0200

    PHPDoc comments

--- src/ArrayUtils.php
+++ src/ArrayUtils.php
@@ -4,6 +4,43 @@ namespace mmedojevicbg\PHPUtils;
 
 class ArrayUtils
 {
+    /**
+     * Builds a nested associative array from a multidimensional array.
+     *
+     * For example,
+     *
+     * ```php
+     * $array = [
+     *     ['id' => '11', 'date' => '2016-05-05', 'count' => 312],
+     *     ['id' => '22', 'date' => '2016-05-05', 'count' => 432],
+     *     ['id' => '33', 'date' => '2016-05-05', 'count' => 653],
+     *     ['id' => '11', 'date' => '2016-05-06', 'count' => 142],
+     *     ['id' => '22', 'date' => '2016-05-06', 'count' => 634],
+     *     ['id' => '33', 'date' => '2016-05-06', 'count' => 912],
+     * ];
+     *
+     * $result = ArrayUrils::multidimensionalToNested($array);
+     * // the result is:
+     * // [
+     * //     '11' => [
+     * //       '2016-05-05' => 312,
+     * //       '2016-05-06' => 142,
+     * //     ],
+     * //     '22' => [
+     * //       '2016-05-05' => 432,
+     * //       '2016-05-06' => 634,
+     * //     ],
+     * //     '33' => [
+     * //       '2016-05-05' => 653,
+     * //       '2016-05-06' => 912,
+     * //     ]
+     * // ]
+     * ```
+     *
+     * @param array $array
+     * @param bool $valueAsArray
+     * @return array
+     */
     public static function multidimensionalToNested(array $array, $valueAsArray = false) {
         $result = [];
         $array = array_values($array);
